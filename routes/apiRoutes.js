5d10fe4854726e46e29dce55a6b68b136bb4a9ea
Mariah <mariahjohnston217@outlook.com>
2022-07-26 15:40:43 -0400

    finished modularizing server.js into apiRoutes.js file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,35 @@
 const router = require('express').Router()
 
+app.get('/api/notes', (req,res) => res.send(db))
 
+
+//add new note
+app.post('/api/notes',(req,res)=>{
+    const {title, text} = req.body
+    console.log(req.body)
+    if (title !="" && text !="") {
+        const newNote ={
+            title,
+            text,
+            note_id: uuid(),
+        };
+
+        fs.readFile('./db/db.json', 'utf8', (err, data) =>{
+            if (err) {
+                console.error(err)
+            } else {
+                const parsedNotes = JSON.parse(data)
+            
+                parsedNotes.push(newNote)
+                fs.writeFileSync('./db/db.json', JSON.stringify(parsedNotes, null, 4),(writeErr)=>
+                writeErr
+                ? console.error(writeErr)
+                : console.info('Success'))
+            }
+           //need to find a way to save note without refreshing page
+        })
+    }
+})
 
 
 module.exports=router
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,12 +3,12 @@ const path = require('path')
 
 //notes page
 router.get('/notes', (req, res)=>{
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
 })
 
 //wild card/home page
 router.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
 module.exports=router
