679413b4dfa350f8099195ae3f78676ef8b2a4d9
Mariah <mariahjohnston217@outlook.com>
2022-07-26 21:18:02 -0400

    deleted apiroute and html route and moved all back under server.js, fixed note_id to id which fixed bug

--- server.js
+++ server.js
@@ -1,16 +1,60 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3001
-const htmlRoute = require('./routes/htmlRoutes')
-const apiRoute = require('./routes/apiRoutes')
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const PORT = process.env.PORT || 3001;
+const path = require("path");
+const db = require("./db/db.json");
+const uuid = require("./helpers/uuid");
 
-app.use(express.json())
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
 
-app.use(apiRoute)
-app.use(htmlRoute)
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, () =>{
-    console.log("App is listening on port http://localhost:"+ PORT)
+//home page
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+//notes page
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+    res.send(db);
+});
+
+//add new note
+app.post("/api/notes", (req, res) => {
+    const { title, text, } = req.body;
+    if (title != "" && text != "") {
+        const newNote = {
+            id: uuid(),
+            title,
+            text
+        }
+
+        db.push((newNote));
+        const noteString = JSON.stringify(db, null, "\t")
+
+        fs.writeFile("./db/db.json", noteString, (err) =>
+                err
+                    ? console.error(err)
+                    : console.log("Success")
+        )
+    }
+    res.json(db)
+})
+
+//delete note
+app.delete()
+
+//wild card
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+})
+
+app.listen(PORT, () => {
+    console.log("App is listening on port http://localhost:" + PORT)
 })
